a0893b21f416f51540976225fbd3d2afb852a02d
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-06-05 22:45:35 +0530

    [Pankaj] added single cast delegate

---
+++ Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate int operation(int a, int b);
+    internal class Program
+    {
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate POC");
+
+            operation obj = new operation (Addition);
+
+            Console.WriteLine("Addition is {0}",obj(10, 20));
+
+        }
+    }
+}
